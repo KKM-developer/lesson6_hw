9b0610ab84c353cb95dd9c5eefa393de1c05c70e
kirill kurenev <cirill@mail.ru>
2022-09-06 22:40:23 +0300

    first task

--- Program.cs
+++ Program.cs
@@ -3,6 +3,21 @@
 0, 7, 8, -2, -2 -> 2
 1, -7, 567, 89, 223-> 3
 */
+int moreThenNull()
+{
+    Console.Write("Введите целое число: ");
+    string numb = Console.ReadLine();
+    int count = 0;
+    while (numb != "q")
+    {
+        int number = Convert.ToInt32(numb);
+        if (number>0) count++;
+        Console.Write("Введите целое число (для выхода введите 'q'): ");
+        numb = Console.ReadLine();
+    }
+    return count;
+}
+Console.WriteLine($"Количество чисел, которые вы ввели больше нуля = {moreThenNull()}");
 /*
 Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
 b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
